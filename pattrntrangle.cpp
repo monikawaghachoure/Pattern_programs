46570cb898541982b715495abff1046f5102487c
monikawaghachoure <monika.omx@gmail.com>
2024-05-20 00:09:46 +0530

    Create pattrntrangle.cpp

---
+++ pattrntrangle.cpp
@@ -0,0 +1,35 @@
+package labassinment;
+
+public class pattrntrangle {
+
+	public static void main(String[] args)
+	{
+
+		        int i, j, k,n=6;
+
+		        for (i = 1; i <= n; i++)
+		        {
+		            for (j = i; j < n; j++)
+		            {
+		                System.out.print(" ");
+		            }
+		            for (k = 1; k <= (2 * i - 1); k++)
+		            {
+
+		                if (k == 1 || i == n || k == (2 * i - 1))
+		                {
+		                    System.out.print("*");
+		                }
+
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.println("");
+		        }
+		    }
+
+
+
+
+}
